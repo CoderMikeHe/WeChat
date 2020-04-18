5bb546fb967270b45488ed46d13dd9369868dcbd
CoderMikeHe <491273090@qq.com>
2020-04-18 18:22:41 +0800

    ADD UIImage+MHSVG

---
+++ WeChat/WeChat/Utils/Category/UIImage/UIImage+MHSVG.m
@@ -0,0 +1,38 @@
+//
+//  UIImage+MHSVG.m
+//  WeChat
+//
+//  Created by admin on 2020/4/18.
+//  Copyright © 2020 CoderMikeHe. All rights reserved.
+//
+
+// 针对SVGKit 做拓展
+// 参照👉: https://www.jianshu.com/p/6b82beae0379
+
+#import "UIImage+MHSVG.h"
+
+@implementation UIImage (MHSVG)
+/**
+ show svg image
+ 
+ @param name svg name
+ @param targetSize image size
+ @return svg image
+ */
++ (UIImage *)mh_svgImageNamed:(NSString *)name targetSize:(CGSize)size{
+    return nil;
+}
+
+
+/**
+ show svg image
+ 
+ @param name       svg name
+ @param targetSize image size
+ @param tintColor  image color
+ @return svg image
+ */
++ (UIImage *)mh_svgImageNamed:(NSString *)name targetSize:(CGSize)size tintColor:(UIColor *)tintColor{
+    return nil;
+}
+@end
